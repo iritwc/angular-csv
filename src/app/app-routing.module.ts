f2cea81cd95b40fe3ad9a0d1f5997a54a33dc36b
Irit <15170559+iritwc@users.noreply.github.com>
2023-04-10 06:15:13 +0300

    Add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GcdComponent } from './gcd/gcd.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: GcdComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
